03d7bf89d6c154fa11a5aaf77b4543c63a99df52
Nandini Chhajed <61181640+nandinichhajed@users.noreply.github.com>
2022-10-27 18:10:52 +0530

    Row sum

---
+++ rowsum.cpp
@@ -0,0 +1,60 @@
+
+#include <iostream>
+using namespace std;
+
+// Take input for 2D Array
+void takeInput(int arr[][3], int row, int col)
+{
+    cout << "Enter the value of 3*3 array: ";
+
+    // Taking input
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+}
+
+// Traversing the 2D Array
+void traverse(int arr[][3], int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+// Row wise sum
+void rowWiseSum(int arr[][3], int row, int col)
+{
+    cout << "Row Wise sum: ";
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            sum += arr[i][j];
+        }
+        cout << sum << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    int arr[3][3];
+    takeInput(arr, 3, 3);
+    traverse(arr, 3, 3);
+
+    // For printing row wise sum
+    rowWiseSum(arr, 3, 3);
+
+    return 0;
+}
